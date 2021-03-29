92103acdede51e876e9694f0e486e8c8d8fb2347
PePacheco <pedro_pacheco98@outlook.com>
2021-03-29 15:10:38 -0300

    First commit

---
+++ Config/ConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigApp.swift
+//  Config
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Config/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Config
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
