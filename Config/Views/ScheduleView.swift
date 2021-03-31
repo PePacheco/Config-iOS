d49012c3cdf346126704b9ee3e61f08774daa333
PePacheco <pedro_pacheco98@outlook.com>
2021-03-31 13:56:49 -0300

    Removing the duplicate navigationview

--- Config/Views/ScheduleView.swift
+++ Config/Views/ScheduleView.swift
@@ -9,21 +9,19 @@ import SwiftUI
 
 struct ScheduleView: View {
     var body: some View {
-        NavigationView {
-            List {
-                SearchBar()
-                Text("Wednesday, April 21st")
-                    .font(Font.custom("San-Francisco", size: 25.0))
-                    .fontWeight(.bold)
-                    .padding(.top, 20)
-                
-                PresentationRow(presentations: [DataFile.shared.presentationsData[0]], date: "1:00-1:40pm BRT")
-                PresentationRow(presentations: [DataFile.shared.presentationsData[1], DataFile.shared.presentationsData[2]], date: "1:50-2:20pm BRT")
-                PresentationRow(presentations: [DataFile.shared.presentationsData[4],DataFile.shared.presentationsData[3]], date: "2:30-3:00pm BRT")
-            }
-            .navigationTitle("Schedule")
-            .background(Color.blue)
+        List {
+            SearchBar()
+            Text("Wednesday, April 21st")
+                .font(Font.custom("San-Francisco", size: 25.0))
+                .fontWeight(.bold)
+                .padding(.top, 20)
+            
+            PresentationRow(presentations: [DataFile.shared.presentationsData[0]], date: "1:00-1:40pm BRT")
+            PresentationRow(presentations: [DataFile.shared.presentationsData[1], DataFile.shared.presentationsData[2]], date: "1:50-2:20pm BRT")
+            PresentationRow(presentations: [DataFile.shared.presentationsData[4],DataFile.shared.presentationsData[3]], date: "2:30-3:00pm BRT")
         }
+        .navigationTitle("Schedule")
+        .background(Color.blue)
     }
 }
 
