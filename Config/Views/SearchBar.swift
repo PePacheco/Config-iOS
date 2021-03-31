9ba2a53562175a415244f73e9fc2ba7c02df9b0f
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-03-31 09:14:53 -0300

    fixing conflicts in searchbar

--- Config/Views/SearchBar.swift
+++ Config/Views/SearchBar.swift
@@ -24,41 +24,9 @@ struct SearchBar: View {
                 .cornerRadius(8)
             
             HStack(spacing: 14) {
-<<<<<<< HEAD
                 Filter(color: UIColor.green, title: "Embrassing the mess", width: 140)
                 Filter(color: UIColor.purple, title: "Shifting the culture", width: 130)
                 Filter(color: UIColor.yellow, title: "In the file", width: 80)
-=======
-                HStack(alignment: .center) {
-                    Text(" Embracing the mess ")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                }
-                    .frame(width: 130, height: 30)
-                    .background(Color(UIColor.green))
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.black, lineWidth: 3))
-                    
-                HStack(alignment: .center) {
-                    Text(" Shifting the culture ")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                }
-                    .frame(width: 130, height: 30)
-                    .background(Color(UIColor.purple))
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.black, lineWidth: 3))
-                
-                HStack(alignment: .center) {
-                    Text("In the file")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                }
-                    .frame(width: 100, height: 30)
-                    .background(Color(UIColor.yellow))
-                    .cornerRadius(16)
-                    .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.black, lineWidth: 3))
->>>>>>> 5ab8d59a4beeecfd9012cbb39351fe661073b44f
             }
         }
     }
