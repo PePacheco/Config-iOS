fe5ae865cad039b35964a1f7a5692b213889a8bf
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-03-31 17:46:53 -0300

    SpeakerDetails

---
+++ Config/Views/SpeakerDetailPresentationView.swift
@@ -0,0 +1,52 @@
+//
+//  SpeakerDetailPresentationView.swift
+//  Config
+//
+//  Created by Anderson Sprenger on 31/03/21.
+//
+
+import SwiftUI
+
+struct SpeakerDetailPresentationView: View {
+    var presentation: Presentation
+    
+    var body: some View {
+        let names: String = presentation.speakers.reduce("", { $0 + $1.name + ", "})
+        
+        VStack {
+            Text("data formatada")
+                .font(Font.custom("RobotoMono-Bold", size: 20))
+                .kerning(-0.64)
+                .frame(width: 365, height: 48, alignment: .center)
+                .background(Color.blue)
+                .border(Color.black, width: 4)
+                //.offset(y: 0)
+            VStack(alignment: .leading, spacing: 8) {
+                
+                Text(presentation.title)
+                    .font(Font.custom("RobotoMono-Bold", size: 16))
+                    .kerning(-0.64)
+                    
+                Text(names)
+                    .font(Font.custom("Inter-Regular", size: 12))
+                    .foregroundColor(.gray)
+                //Spacer()
+            }
+            .padding(.vertical, 4)
+            .padding(.horizontal, 8)
+
+                //.padding(.top, 20)
+            Spacer()
+        }
+            .frame(width: 365, height: 202)
+            //.border(width: 25, edges: [.top], color: switchColors(color: presentation.bannerName))
+            .border(Color.black, width: 4)
+        
+    }
+}
+
+struct SpeakerDetailPresentationView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakerDetailPresentationView(presentation: DataFile.shared.presentationsData[0])
+    }
+}
---
+++ Config/Views/SpeakerDetails.swift
@@ -0,0 +1,53 @@
+//
+//  SpeakerDetails.swift
+//  Config
+//
+//  Created by Anderson Sprenger on 31/03/21.
+//
+
+import SwiftUI
+
+struct SpeakerDetails: View {
+    var speaker: Speaker
+    
+    var body: some View {
+        ScrollView (.vertical, showsIndicators: false) {
+            VStack (alignment: .center, spacing: nil) {
+                HStack {
+                    SpeakerItemInternal(speaker)
+                    Spacer()
+                }
+                Text(speaker.description)
+                    .multilineTextAlignment(.leading)
+                    .font(Font.custom("Inter-Medium", size: 17))
+                    .padding(.top, 24)
+                
+                Divider()
+                HStack {
+                    Text("Keynotes")
+                        .font(Font.custom("Inter-Bold", size: 24))
+                        .kerning(-0.64)
+                        .padding(.vertical, 15)
+                    Spacer()
+                }
+                VStack {
+                    ForEach (DataFile.shared.presentationsData) { presentation in
+                        if presentation.speakers.contains(where: { (pearson) -> Bool in
+                            speaker.id == pearson.id
+                        }) {
+                            SpeakerDetailPresentationView(presentation: presentation)
+                                .padding(.all, 10)
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct SpeakerDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakerDetails(speaker: DataFile.shared.speakersData[1])
+    }
+}
